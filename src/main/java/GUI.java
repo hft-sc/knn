a725bb57cc503c9db636ca1306ac0ee83429d48b
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2021-05-10 12:31:24 +0200

    Remove redundant new lines and format project

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,8 +1,3 @@
-import java.awt.Color;
-//import java.awt.Shape;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingUtilities;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -12,86 +7,88 @@ import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GUI {
-	
-	public static void zeichnen(double[][] daten, double[][] flaeche){
-		
-		 JFrame f     = new JFrame();
-         JLabel label = new JLabel("Loading ...");
-         label.setBounds(260,250, 80,40);
-         f.add(label);        
-         f.setSize(600,600);
-         f.setLayout(null);
-         f.setVisible(true);
-         JFreeChart chart;
-        
-	     chart            = testcreateChart(daten, flaeche); 
-	     ChartPanel panel = new ChartPanel(chart);
-	     panel.setPreferredSize(f.getSize());
-	     f.setContentPane(panel);
-	     SwingUtilities.updateComponentTreeUI(f);	
-		
-	}
-	
-	 private static JFreeChart testcreateChart(double[][] daten, double[][] flaeche) {
-		 
-	    	
-	    XYSeries hyperplane   = areaSeries (flaeche,    "Flaeche");
-	    XYSeries class1points = pointSeries(daten  , 0, "KlasseNegativ");
-	    XYSeries class2points = pointSeries(daten  , 1, "KlassePositiv");
 
-	    XYSeriesCollection dataset = new XYSeriesCollection();
-	    dataset.addSeries(class1points);
-	    dataset.addSeries(class2points);
-	    dataset.addSeries(hyperplane);
+    public static void zeichnen(double[][] daten, double[][] flaeche) {
 
-	    JFreeChart chart = ChartFactory.createXYLineChart("", "", "", dataset, PlotOrientation.VERTICAL, false, false, false);
-	        
+        JFrame f = new JFrame();
+        JLabel label = new JLabel("Loading ...");
+        label.setBounds(260, 250, 80, 40);
+        f.add(label);
+        f.setSize(600, 600);
+        f.setLayout(null);
+        f.setVisible(true);
+        JFreeChart chart;
 
-	    XYPlot plot = (XYPlot) chart.getPlot();
-	    XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-	   //HOMchart.getPlot().setBackgroundPaint( Color.WHITE );
+        chart = testcreateChart(daten, flaeche);
+        ChartPanel panel = new ChartPanel(chart);
+        panel.setPreferredSize(f.getSize());
+        f.setContentPane(panel);
+        SwingUtilities.updateComponentTreeUI(f);
 
-	        
-	    renderer.setSeriesLinesVisible(1, false);
-	    renderer.setSeriesShapesVisible(1, true);
-	        
-	    renderer.setSeriesPaint(1, Color.red);
-	    renderer.setSeriesLinesVisible(2, false);
-	    renderer.setSeriesShapesVisible(2, true);
-	        
-	    renderer.setSeriesPaint(2, Color.yellow);
-	   //HOMrenderer.setSeriesPaint(2, Color.GRAY);
-	        
+    }
+
+    private static JFreeChart testcreateChart(double[][] daten, double[][] flaeche) {
+
+
+        XYSeries hyperplane = areaSeries(flaeche, "Flaeche");
+        XYSeries class1points = pointSeries(daten, 0, "KlasseNegativ");
+        XYSeries class2points = pointSeries(daten, 1, "KlassePositiv");
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(class1points);
+        dataset.addSeries(class2points);
+        dataset.addSeries(hyperplane);
+
+        JFreeChart chart = ChartFactory.createXYLineChart("", "", "", dataset, PlotOrientation.VERTICAL, false, false, false);
+
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        //HOMchart.getPlot().setBackgroundPaint( Color.WHITE );
+
+
+        renderer.setSeriesLinesVisible(1, false);
+        renderer.setSeriesShapesVisible(1, true);
+
+        renderer.setSeriesPaint(1, Color.red);
+        renderer.setSeriesLinesVisible(2, false);
+        renderer.setSeriesShapesVisible(2, true);
+
+        renderer.setSeriesPaint(2, Color.yellow);
+        //HOMrenderer.setSeriesPaint(2, Color.GRAY);
+
 //	    Shape s = renderer.getSeriesShape(1);
-	    renderer.setSeriesLinesVisible(0, false);
-	    renderer.setSeriesShapesVisible(0, true);
-	    renderer.setSeriesPaint(0, Color.blue);
-	    plot.setRenderer(renderer);
-	        
-	        
-	    return chart;
-	 }
+        renderer.setSeriesLinesVisible(0, false);
+        renderer.setSeriesShapesVisible(0, true);
+        renderer.setSeriesPaint(0, Color.blue);
+        plot.setRenderer(renderer);
 
-	 
-	 
-	 private static XYSeries areaSeries(double[][] flaeche, String key) {
-	     XYSeries series = new XYSeries(key);
-	     for (int i=0;i<flaeche.length;i++) {
-	       	double wert1 = flaeche[i][0];
-	       	double wert2 = flaeche[i][1];
-       		series.add(wert1, wert2);
-	     }        
-	     return series;
-	 }	 
-	 
-	 private static XYSeries pointSeries(double[][] vectors, int y, String key) {
-		 XYSeries series = new XYSeries(key);
-		 for (int i=0;i<vectors.length;i++) {
-			 if ((int)vectors[i][2] == y) {
-				 series.add(100.*vectors[i][0], 100*vectors[i][1]);//es wird wieder angenommen, dass die Eingabewerte durch 100 geteilt wurden
-	         }
-	     }
-	     return series;
-	 }	
+
+        return chart;
+    }
+
+
+    private static XYSeries areaSeries(double[][] flaeche, String key) {
+        XYSeries series = new XYSeries(key);
+        for (int i = 0; i < flaeche.length; i++) {
+            double wert1 = flaeche[i][0];
+            double wert2 = flaeche[i][1];
+            series.add(wert1, wert2);
+        }
+        return series;
+    }
+
+    private static XYSeries pointSeries(double[][] vectors, int y, String key) {
+        XYSeries series = new XYSeries(key);
+        for (int i = 0; i < vectors.length; i++) {
+            if ((int) vectors[i][2] == y) {
+                series.add(100. * vectors[i][0], 100 * vectors[i][1]);//es wird wieder angenommen, dass die Eingabewerte durch 100 geteilt wurden
+            }
+        }
+        return series;
+    }
 }
--- src/main/java/Hauptprogramm.java
+++ src/main/java/Hauptprogramm.java
@@ -2,39 +2,30 @@ import java.io.File;
 
 public class Hauptprogramm {
 
-	public static void main(String[] args) {
-
-
-
+    public static void main(String[] args) {
 //		double[][] daten = Einlesen.einlesenBankdaten(new File("4_Trainingsdaten.csv"));
 //		double[][] daten = Einlesen.einlesenDiabetes(new File("diabetes.csv"), true);
 //		double[][] daten = Einlesen.einlesenVorlesungsbeispiele(new File("svmKnearestNick.txt"));
-		double[][] daten = Einlesen.einlesenVorlesungsbeispiele(new File("wetter.txt"));
+        double[][] daten = Einlesen.einlesenVorlesungsbeispiele(new File("wetter.txt"));
 //		double[][] daten = Einlesen.einlesenVorlesungsbeispiele(new File("XOR.txt"));
-		int dimension    = daten[0].length-1;
+        int dimension = daten[0].length - 1;
 
-		//Einlesen.auslesen(daten);
-		
-   		int[] strukturNN = {5};//anzahl Knoten (incl. Bias) pro Hiddenschicht
-		KNN netz   = new KNN(dimension, strukturNN);
+        //Einlesen.auslesen(daten);
 
+        int[] strukturNN = {5};//anzahl Knoten (incl. Bias) pro Hiddenschicht
+        KNN netz = new KNN(dimension, strukturNN);
 
+        netz.trainieren(daten);//Verlustfunktion min
+        //netz.trainierenStochastisch(daten);
+        //netz.trainierenMiniBatch(daten);
+        //netz.trainierenBatch(daten);
 
-		netz.trainieren(daten);//Verlustfunktion min
-		//netz.trainierenStochastisch(daten);
-		//netz.trainierenMiniBatch(daten);
-		//netz.trainierenBatch(daten);
-
-		
 //		daten = Einlesen.einlesenBankdaten(new File("test01.csv")); 	
 //		daten = Einlesen.einlesenBankdaten(new File("5_Testdaten.csv"));
 //		daten = Einlesen.einlesenDiabetes(new File("diabetes.csv"), false);
-		//Einlesen.auslesen(daten);
+        //Einlesen.auslesen(daten);
 //		netz.evaluieren(daten);
-		
-		
-	    netz.evaluierenGUIII(daten);
-	
-	}
 
+        netz.evaluierenGUIII(daten);
+    }
 }
