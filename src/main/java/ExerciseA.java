a7990cd6f63035a2c0edfb42300cc146fa09ef44
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2021-06-06 10:32:31 +0200

    Disable printing of reading bank data

--- src/main/java/ExerciseA.java
+++ src/main/java/ExerciseA.java
@@ -13,7 +13,7 @@ public class ExerciseA {
     };
 
     public static void main(String[] args) {
-        double[][] daten = Einlesen.einlesenBankdaten(new File("4_Trainingsdaten.csv"), true);
+        double[][] daten = Einlesen.einlesenBankdaten(new File("4_Trainingsdaten.csv"), false);
 //        double[][] daten = Einlesen.einlesenDiabetes(new File("diabetes_train.csv"), true, false);
         int dimension = daten[0].length - 1;
 
@@ -22,7 +22,7 @@ public class ExerciseA {
 
             netz.trainieren(daten, false);//Verlustfunktion min
 
-            daten = Einlesen.einlesenBankdaten(new File("test01.csv"), true);
+            daten = Einlesen.einlesenBankdaten(new File("test01.csv"), false);
 //            daten = Einlesen.einlesenDiabetes(new File("diabetes_test.csv"), false, false);
             var result = netz.evaluieren(daten);
             Utils.printResult(parameters, result);
